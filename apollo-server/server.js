5bd68edc5fd1ba4a701ea218bbc93e01ef4affbc
Dalibor Moc <dalibor.moc@packwise.de>
2020-06-30 12:42:42 +0200

    delete files

--- apollo-server/server.js
+++ apollo-server/server.js
@@ -1,6 +1,4 @@
 import path from 'path'
 import express from 'express'
 
-export default app => {
-  // app.use('/files', express.static(path.resolve(__dirname, '../live/uploads')))
-}
+export default app => {}
